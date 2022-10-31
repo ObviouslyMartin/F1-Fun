2b10a109f8f3776638dd658b35dfee6832c09a2b
martin plut <mjplut@hotmail.com>
2022-10-31 12:05:59 -0700

    functional EW class

--- pointsCalculator.py
+++ pointsCalculator.py
@@ -9,6 +9,14 @@ f1 = ergast()
 
 class Earliest_Win:
 
+    def __init__(self):
+        self.num_races, self.num_sprint_races = self.get_num_races()
+        self.max_points = (self.num_races * 26) + (self.num_sprint_races * 8)
+        self.leader_points = self.get_champ_leader_points()
+        self.second_place_points = self.get_second_place_points()
+        self.print()
+
+
     def get_num_races(self, season="current") -> int:
         race_weekends = f1.getSeasonLength()
         sprint_races = self.find_sprints(race_weekends[1]["MRData"]["RaceTable"]["Races"])
@@ -24,36 +32,30 @@ class Earliest_Win:
                 continue
         return sprints
 
-    def get_remaining_points(self, season="current"):
+    def get_remaining_points(self, season="current") -> int:
         '''
         returns the amount of points left to be won during the season.
         requires <remaining races> x <number of points to be won>
         '''
         pass
-    def get_champ_leader_points(season="current"):
+    def get_champ_leader_points(self, season="current") -> int:
         '''
         return leaders points at the current moment
         '''
         return f1.getDriverStandings()[0]["points"]
 
-    def get_second_place_points(season="current"):
+    def get_second_place_points(self, season="current") -> int:
         '''
         return second place's points at the current moment
         '''
         return f1.getDriverStandings()[1]["points"]
 
-    def calculate(self):
-        pass
-    
-    def init(self):
-        self.num_races, self.num_sprint_races = self.get_num_races()
-        self.max_points = (self.num_races * 26) + (self.num_sprints * 8)
-        self.leader_points = self.get_champ_leader_points()
-        self.second_place_points = self.get_second_place_points()
-        self.calculate()
-
-
-
+    def print(self):
+        print(f'number of races in the current season: {self.num_races}')
+        print(f'number of sprint races in the current season: {self.num_sprint_races}')
+        print(f'maximum number of points in the current season: {self.max_points}')
+        print(f'championship leader current points total: {self.leader_points}')
+        print(f'second place current points total: {self.second_place_points}')
 
 
 
@@ -62,12 +64,6 @@ def earliest_win():
     return the race where second place can no longer win enough points to beat first place (remaining points < delta between 1st and 2nd). 
     '''
     win = Earliest_Win()
-    print("done")
-    # max_points = win.get_max_points()
-    # remaining_points= get_remaining_points(max_points)
-    # first_place_points = get_champ_leader_points()
-    # second_place_points = get_second_place_points()
-    # points_delta = first_place_points - second_place_points            
 
     
 if __name__ == '__main__':
